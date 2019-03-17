6040901c228e84086e8fd47c35ae3a9d92f26131
davidduplantier <dduplant@ucsd.edu>
2019-03-17 18:21:59 -0400

    Create new homepage

---
+++ game.js
@@ -0,0 +1,13 @@
+function run() {
+  while (true) {
+    input();
+    updateState();
+    requestAnimationFrame(render);
+  }
+}
+
+function input() {}
+
+function updateState() {}
+
+function render() {}
